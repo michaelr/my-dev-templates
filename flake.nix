66d427e9a6ffe18826dd2871daad177c02804b5c
Michael Reddick <michael.reddick@gmail.com>
2022-10-04 15:17:50 -0500

    Create initial flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description =
+    "Flake based dev templates. Based on Luc Perkins - github:the-nix-way/dev-templates";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    {
+      templates = {
+        elixir = {
+          path = ./elixir;
+          description = "Elixir development environment";
+        };
+
+      };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) mkShell writeScriptBin;
+        exec = pkg: "${pkgs.${pkg}}/bin/${pkg}";
+
+        format = writeScriptBin "format" ''
+          ${exec "nixpkgs-fmt"} **/*.nix
+        '';
+
+        update = writeScriptBin "update" ''
+          for dir in `ls -d */`; do # Iterate through all the templates
+            (
+              cd $dir
+              ${exec "nix"} flake update # Update flake.lock
+              ${
+                exec "direnv"
+              } reload    # Make sure things work after the update
+            )
+          done
+        '';
+      in
+      {
+        devShells = { default = mkShell { buildInputs = [ format update ]; }; };
+      });
+}
