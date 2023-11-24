28a7b7a659fc23416ee745a42436b121287e1771
Michael Reddick <michael.reddick@gmail.com>
2023-11-24 13:55:41 -0600

    Add db convenience scripts

--- phoenix/flake.nix
+++ phoenix/flake.nix
@@ -15,10 +15,29 @@
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
+      inherit (pkgs) mkShell writeScriptBin;
+      exec_bin = pkg: bin: "${pkgs.${pkg}}/bin/${bin}";
+
+      db_up = writeScriptBin "db_up" ''
+        #!/usr/bin/env bash
+        ${exec_bin "postgresql" "pg_ctl"} -D ".direnv/postgres" -l ".direnv/postgres/postgres.log" start
+        '';
+
+      db_down = writeScriptBin "db_down" ''
+        #!/usr/bin/env bash
+        ${exec_bin "postgresql" "pg_ctl"} stop
+        '';
+
+      db_log = writeScriptBin "db_log" ''
+        #!/usr/bin/env bash
+        ${exec_bin "tailspin" "tspin"} -f .direnv/postgres/postgres.log
+        '';
+
     in
     {
       devShell = pkgs.mkShell {
         buildInputs = (with pkgs; [ elixir sd postgresql esbuild ]) ++
+          [ db_up db_down db_log ] ++
           pkgs.lib.optionals (pkgs.stdenv.isLinux) (with pkgs; [ gigalixir inotify-tools libnotify ]) ++ # Linux only
           pkgs.lib.optionals (pkgs.stdenv.isDarwin) ((with pkgs; [ terminal-notifier ]) ++ # macOS only
             (with pkgs.darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]));
