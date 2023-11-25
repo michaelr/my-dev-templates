72ebcecebb6e77e9677acaaadb61ddb43598700e
Michael Reddick <michael.reddick@gmail.com>
2023-11-24 20:06:24 -0600

    Add project init script

--- phoenix/flake.nix
+++ phoenix/flake.nix
@@ -33,11 +33,36 @@
         ${exec_bin "tailspin" "tspin"} -f .direnv/postgres/postgres.log
         '';
 
+      proj_init = writeScriptBin "proj_init" ''
+        #!/usr/bin/env bash
+        set -eu
+        if [ -d .mix ]; then
+          echo ".mix/ already exists. $0 was probably already run. Exiting."
+          exit 1
+        fi
+
+        ${exec_bin "elixir" "mix"} local.hex --force
+        ${exec_bin "elixir" "mix"} archive.install hex phx_new --force
+
+        yes 2>/dev/null | ${exec_bin "elixir" "mix"} phx.new . --live
+
+        # setup dev config to use $PGDATA to connect to the database
+        ${exec_bin "sd" "sd"} -f m \
+            'username: "postgres",\n\s+password: "postgres",\n\s+hostname: "localhost",' \
+            'socket_dir: System.get_env("PGDATA"),' \
+            config/dev.exs
+
+        db_up # start postgres
+
+        ${exec_bin "elixir" "mix"} setup
+        ${exec_bin "elixir" "mix"} phx.server
+        '';
+
     in
     {
       devShell = pkgs.mkShell {
-        buildInputs = (with pkgs; [ elixir sd postgresql esbuild ]) ++
-          [ db_up db_down db_log ] ++
+        buildInputs = (with pkgs; [ elixir postgresql esbuild ]) ++
+          [ db_up db_down db_log proj_init ] ++
           pkgs.lib.optionals (pkgs.stdenv.isLinux) (with pkgs; [ gigalixir inotify-tools libnotify ]) ++ # Linux only
           pkgs.lib.optionals (pkgs.stdenv.isDarwin) ((with pkgs; [ terminal-notifier ]) ++ # macOS only
             (with pkgs.darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]));
@@ -45,6 +70,7 @@
         shellHook = ''
           ${pkgs.elixir}/bin/mix --version
           ${pkgs.elixir}/bin/iex --version
+          export MIX_HOME=$(pwd)/.mix
         '';
       };
     });
