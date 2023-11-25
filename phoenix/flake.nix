eeee8222928410d9fa6f4ea2c33126409123104e
Michael Reddick <michael.reddick@gmail.com>
2023-11-25 09:59:04 -0600

    Get rid of exec_bin

--- phoenix/flake.nix
+++ phoenix/flake.nix
@@ -16,21 +16,20 @@
     let
       pkgs = import nixpkgs { inherit system; };
       inherit (pkgs) mkShell writeScriptBin;
-      exec_bin = pkg: bin: "${pkgs.${pkg}}/bin/${bin}";
 
       db_up = writeScriptBin "db_up" ''
         #!/usr/bin/env bash
-        ${exec_bin "postgresql" "pg_ctl"} -D ".direnv/postgres" -l ".direnv/postgres/postgres.log" start
+        ${pkgs.postgresql}/bin/pg_ctl -D ".direnv/postgres" -l ".direnv/postgres/postgres.log" start
         '';
 
       db_down = writeScriptBin "db_down" ''
         #!/usr/bin/env bash
-        ${exec_bin "postgresql" "pg_ctl"} stop
+        ${pkgs.postgresql}/bin/pg_ctl stop
         '';
 
       db_log = writeScriptBin "db_log" ''
         #!/usr/bin/env bash
-        ${exec_bin "tailspin" "tspin"} -f .direnv/postgres/postgres.log
+        ${pkgs.tailspin}/bin/tspin -f .direnv/postgres/postgres.log
         '';
 
       proj_init = writeScriptBin "proj_init" ''
@@ -41,21 +40,21 @@
           exit 1
         fi
 
-        ${exec_bin "elixir" "mix"} local.hex --force
-        ${exec_bin "elixir" "mix"} archive.install hex phx_new --force
+        ${pkgs.elixir}/bin/mix local.hex --force
+        ${pkgs.elixir}/bin/mix archive.install hex phx_new --force
 
-        yes 2>/dev/null | ${exec_bin "elixir" "mix"} phx.new . --live
+        yes 2>/dev/null | ${pkgs.elixir}/bin/mix phx.new . --live
 
         # setup dev config to use $PGDATA to connect to the database
-        ${exec_bin "sd" "sd"} -f m \
+        ${pkgs.sd}/bin/sd -f m \
             'username: "postgres",\n\s+password: "postgres",\n\s+hostname: "localhost",' \
             'socket_dir: System.get_env("PGDATA"),' \
             config/dev.exs
 
         db_up # start postgres
 
-        ${exec_bin "elixir" "mix"} setup
-        ${exec_bin "elixir" "mix"} phx.server
+        ${pkgs.elixir}/bin/mix setup
+        ${pkgs.elixir}/bin/mix phx.server
         '';
 
     in
